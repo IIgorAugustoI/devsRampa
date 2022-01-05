9611a451811a67c0cd9cb201eb0581df77af63e5
IIgorAugustoI <76701857+IIgorAugustoI@users.noreply.github.com>
2022-01-05 09:55:23 -0300

    Adicionado interação com as tabelas

--- tabela-casal/main.js
+++ tabela-casal/main.js
@@ -1,18 +1,55 @@
-function adicionaPessoa(nome, despesa,pago, valor, saldo) {
+var calc = 0;
+var conjujes = [];
 
-    var tabela = document.getElementById("tabela"); // Pegar o elemento no html pelo seu ID.
-    var qtdLinha = tabela.rows.length; // Para saber a quantidade de linhas da tabela
-    var linha = tabela.insertRow(qtdLinha); // Adicionar mais uma linha a tabela.
+alert("Adicione as informações do primeiro Cônjuge");
 
-    var celulaNome = linha.insertCell(0);
-    var celulaDespesa = linha.insertCell(1);
-    var celulaPago = linha.insertCell(2);
-    var celulaValor = linha.insertCell(3);
-    var celulaSaldo = linha.insertCell(4);
+function adicionaPessoa(despesa, pago , valor) {
+    conjujes.push([Number(despesa), Number(pago), Number(valor)]);
+        ++calc;
+    if (calc > 1) {
+        Calculos();
+    }else {
+    alert("Adicione as informações do segundo Cônjuge");
+    }
+}
 
-    celulaNome.innerHTML = nome;
-    celulaDespesa.innerHTML = despesa;
-    celulaPago.innerHTML = pago;
-    celulaValor.innerHTML = valor;
-    celulaSaldo.innerHTML = saldo;
+function Calculos() {
+    
+    var despesaTotal = Number(conjujes[0][0]) + Number(conjujes[1][0]);
+    conjujes[0][1] = (Number(conjujes[0][0]) / Number(despesaTotal) * 100);
+    
+    conjujes[0].push = conjujes[0][0] - (despesaTotal / 2);
+    
+    conjujes[1][1] = (Number(conjujes[1][0]) / Number(despesaTotal) * 100);
+    var porcentagemConjuge2 = conjujes[1][1];
+    conjujes[1].push = conjujes[1][0] - (despesaTotal / 2);
+    var porcentagemConjuge1 = conjujes[0][1];
+
+    var saldoConjujes1 = conjujes[0][0] - (despesaTotal / 2);
+    var saldoConjujes2 = conjujes[1][0] - (despesaTotal / 2);
+
+    var saldoTotal = saldoConjujes1 + saldoConjujes2;
+    var valorDevidoTotal = (Number(conjujes[0][2]) + Number(conjujes[1][2])) / 2;
+
+    InsetTabela(conjujes, despesaTotal, porcentagemConjuge1, porcentagemConjuge2, valorDevidoTotal, saldoConjujes1, saldoConjujes2, saldoTotal);
+}
+
+function InsetTabela(conjunto,despesaTotal, porcentagemConjuge1, porcentagemConjuge2, valorDevidoTotal,saldoConjujes1, saldoConjujes2, saldoTotal) {
+    // Conjuge 1
+    document.querySelector("#despesa-conjuge1").innerHTML = conjunto[0][0];
+    document.querySelector("#pago-conjuge1").innerHTML = conjunto[0][1];
+    document.querySelector("#valor-conjuge1").innerHTML = conjunto[0][2];
+    document.querySelector("#saldo-conjuge1").innerHTML = saldoConjujes1;
+
+    //Conjuge 2
+
+    document.querySelector("#despesa-conjuge2").innerHTML = conjunto[1][0];
+    document.querySelector("#pago-conjuge2").innerHTML = conjunto[1][1];
+    document.querySelector("#valor-conjuge2").innerHTML = conjunto[1][2];
+    document.querySelector("#saldo-conjuge2").innerHTML = saldoConjujes2;
+
+    document.querySelector("#despesa-total").innerHTML = despesaTotal;
+    document.querySelector("#pago-total").innerHTML = "100";
+    document.querySelector("#valor-total").innerHTML = valorDevidoTotal;
+    document.querySelector("#saldo-total").innerHTML = saldoTotal;
 }
