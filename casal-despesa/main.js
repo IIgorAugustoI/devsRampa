91eb556a261024649698423354f7f01c46703070
IIgorAugustoI <76701857+IIgorAugustoI@users.noreply.github.com>
2022-01-19 20:52:19 -0300

    Add files via upload

---
+++ casal-despesa/main.js
@@ -0,0 +1,13 @@
+function limparcampos(){
+    document.getElementById("input-despesa").value = '';
+    document.getElementById("input-receita").value = '';
+    document.getElementById("input-despesa-form2").value = '';
+    document.getElementById("input-receita-from2").value = '';
+
+    document.getElementById("input-despesa").focus();
+}
+
+var conjujes = [];
+
+function adicionarPessoa(despesa, receita) {
+}
