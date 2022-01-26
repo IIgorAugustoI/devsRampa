5a5b0d6b38fb8bbd8989adfd740b5a4472592753
IIgorAugustoI <76701857+IIgorAugustoI@users.noreply.github.com>
2022-01-25 21:13:56 -0300

    Finalizando cálculos no js

--- casal-despesa/main.js
+++ casal-despesa/main.js
@@ -1,9 +1,9 @@
 function limparCampos(){
-    document.getElementById("despesa").value = '';
-    document.getElementById("receita").value = '';
-    document.getElementById("despesa2").value = '';
-    document.getElementById("receita2").value = '';
-
+    let inputs = document.getElementsByTagName("input");
+    for(const i in inputs){
+        const limpar = inputs[i];
+        limpar.value = "";
+    }
     document.getElementById("despesa").focus();
 }
 
@@ -19,61 +19,65 @@ botaoAdicionar.addEventListener("click", function (event) {
     var despesaConjuge2 = Number(form.despesa2.value);
     var receitaConjuge2 = Number(form.receita2.value);
     var despesaTotal = despesaConjuge1 + despesaConjuge2;
+    var porcentagemPagaConjuge1 = 0;
+    var porcentagemPagaConjuge2 = 0;
     var porcentagemPagaTotal = "100%";
+    var valorDevidoConjuge1 = 0;
+    var valorDevidoConjuge2 = 0;
+    var valorDevidoTotal = 0;
+    var saldoConjuge1 = 0;
+    var saldoConjuge2 = 0;
+    var saldoTotal = "";
+    var resultadoPorcentagem = 0;
 
     var opcoes = document.getElementsByName("radio");
-    for(var i = 0; i < opcoes.length; i++){
-        if(opcoes[0].checked){
-            alert("Você escolheu dividir meio a meio");
+    
+    if(opcoes[0].checked){
+        alert("Você escolheu dividir meio a meio");
+         porcentagemPagaConjuge1 = ((despesaConjuge1 / despesaTotal) * 100).toFixed(2);
+         porcentagemPagaConjuge2 = ((despesaConjuge2 / despesaTotal) * 100).toFixed(2);
+         valorDevidoConjuge1 = (despesaTotal / 2);
+         valorDevidoConjuge2 = (despesaTotal / 2);
+         valorDevidoTotal = valorDevidoConjuge1 + valorDevidoConjuge2;
+         saldoConjuge1 = despesaConjuge1 - valorDevidoConjuge1;
+         saldoConjuge2 = despesaConjuge2 - valorDevidoConjuge2;
 
-            var porcentagemPagaConjuge1 = ((despesaConjuge1 / despesaTotal) * 100).toFixed(2);
-            var porcentagemPagaConjuge2 = ((despesaConjuge2 / despesaTotal) * 100).toFixed(2);
+    } else if (opcoes[1].checked){
+        alert("Você escolheu divisão 60%");
+         porcentagemPagaConjuge1 = 60;
+         porcentagemPagaConjuge2 = 40;
+         valorDevidoConjuge1 = despesaTotal * 0.60;
+         valorDevidoConjuge2 = despesaTotal * 0.40;
+         valorDevidoTotal = valorDevidoConjuge1 + valorDevidoConjuge2;
+         saldoConjuge1 = despesaConjuge1 - valorDevidoConjuge1;
+         saldoConjuge2 = despesaConjuge2 - valorDevidoConjuge2;
 
-        
-            var valorDevidoConjuge1 = (despesaTotal / 2);
-            var valorDevidoConjuge2 = (despesaTotal / 2);
-            var valorDevidoTotal = Number(valorDevidoConjuge1 + valorDevidoConjuge2).toFixed(2);
-        
-            var saldoConjuge1 = despesaConjuge1 - valorDevidoConjuge1;
-            var saldoConjuge2 = despesaConjuge2 - valorDevidoConjuge2;
+    } else if (opcoes[2].checked){
+        alert("Você escolheu proporcionalmente a redenda");
+        if(receitaConjuge1 > receitaConjuge2) {
+            porcentagemPagaConjuge1 = ((receitaConjuge1 * 100) / despesaTotal).toFixed(2);
+            resultadoPorcentagem = ((porcentagemPagaConjuge1 - 100) * (-1)).toFixed(2);
+            porcentagemPagaConjuge2 = resultadoPorcentagem;
 
-            break;
-        } else if (opcoes[1].checked){
-            alert("Você escolheu divisão 60%");
+        } else if (receitaConjuge2 > receitaConjuge1){
+            porcentagemPagaConjuge2 = ((receitaConjuge2 * 100) / despesaTotal).toFixed(2);
+            resultadoPorcentagem = ((porcentagemPagaConjuge2 - 100) * (-1)).toFixed(2);
+            porcentagemPagaConjuge1 = resultadoPorcentagem;
+        }
 
-            var porcentagemPagaConjuge1 = 60;
-            var porcentagemPagaConjuge2 = 40;
-
-            var valorDevidoConjuge1 = despesaTotal * 0.60;
-            var valorDevidoConjuge2 = despesaTotal * 0.40;
-            var valorDevidoTotal = Number(valorDevidoConjuge1 + valorDevidoConjuge2);
-
-            var saldoConjuge1 = despesaConjuge1 - valorDevidoConjuge1;
-            var saldoConjuge2 = despesaConjuge2 - valorDevidoConjuge2;
-
-            break;
-        } else if (opcoes[2].checked){
-            alert("Você escolheu proporcionalmente a redenda");
-
-            var porcentagemPagaConjuge1 = ((receitaConjuge1 / 10) * 100) / despesaTotal;
-            var porcentagemPagaConjuge2 = ((receitaConjuge2 / 10) * 100) / despesaTotal;
-
-            var valorDevidoConjuge1 = (porcentagemPagaConjuge1 / 100) * despesaTotal;
-            var valorDevidoConjuge2 = (porcentagemPagaConjuge2 / 100) * despesaTotal;
-
-            var saldoConjuge1 = Number(despesaConjuge1 - valorDevidoConjuge1);
-            var saldoConjuge2 = Number(despesaConjuge2 - valorDevidoConjuge2);
-            break;
-        } 
-    }
-
-    var saldoTotal = "";
+         valorDevidoConjuge1 = (porcentagemPagaConjuge1 / 100) * despesaTotal;
+         valorDevidoConjuge2 = (porcentagemPagaConjuge2 / 100) * despesaTotal;
+         valorDevidoTotal = valorDevidoConjuge1 + valorDevidoConjuge2;
+         saldoConjuge1 = despesaConjuge1 - valorDevidoConjuge1;
+         saldoConjuge2 = despesaConjuge2 - valorDevidoConjuge2;
+    } 
+    
     if (saldoConjuge1 < saldoConjuge2){
         saldoConjuge2.toFixed(2);
-        saldoTotal = "Cônjuge 1 deve R$ " + saldoConjuge2 + " ao cônjuge 2";
+        saldoTotal = "Cônjuge 1 deve R$ " + saldoConjuge2.toFixed(2) + " ao cônjuge 2";
     } else if (saldoConjuge1 > saldoConjuge2) {
         saldoConjuge1.toFixed(2);
-        saldoTotal = "Cônjuge 2 deve R$ " + saldoConjuge1 + " ao cônjuge 1";
+        saldoTotal = "Cônjuge 2 deve R$ " + saldoConjuge1.toFixed(2) + " ao cônjuge 1";
     } else {
         saldoTotal = "Sem valor a pagar";
     }
@@ -81,8 +85,6 @@ botaoAdicionar.addEventListener("click", function (event) {
     limparCampos();
     escreverTabela(despesaConjuge1, despesaConjuge2, despesaTotal, porcentagemPagaConjuge1, porcentagemPagaConjuge2, porcentagemPagaTotal,valorDevidoConjuge1, valorDevidoConjuge2, valorDevidoTotal ,saldoConjuge1, saldoConjuge2, saldoTotal);
 })
-
-
 
 function escreverTabela(despesaConjuge1, despesaConjuge2, despesaTotal, porcentagemPagaConjuge1, porcentagemPagaConjuge2, porcentagemPagaTotal,valorDevidoConjuge1, valorDevidoConjuge2, valorDevidoTotal ,saldoConjuge1, saldoConjuge2, saldoTotal){
     
@@ -96,7 +98,7 @@ function escreverTabela(despesaConjuge1, despesaConjuge2, despesaTotal, porcenta
 
     document.querySelector("#valor1").textContent = valorDevidoConjuge1.toFixed(2);
     document.querySelector("#valor2").textContent = valorDevidoConjuge2.toFixed(2);
-    document.querySelector("#valor-total").textContent = valorDevidoTotal;
+    document.querySelector("#valor-total").textContent = valorDevidoTotal.toFixed(2);
 
     document.querySelector("#saldo1").textContent = saldoConjuge1.toFixed(2);
     document.querySelector("#saldo2").textContent = saldoConjuge2.toFixed(2);
